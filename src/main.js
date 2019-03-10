25e0832336006c6f1ebd9243a6e8e3c743fc61fa
Irina Ovchinnikova <iovchinnikova@outlook.com>
2019-03-11 00:28:27 +0100

    Добавлены классы

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import {generateFilter} from './generateFilter';
-import {generateCard} from './generateCard';
 import {films} from './data';
+import {Film} from './film';
+import {FilmComments} from './film-comments';
 
 const filterSection = document.querySelector(`.main-navigation`);
-const mainCardContainer = document.querySelector(`.films-list .films-list__container`);
-const cardContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
+const filmsMainContainer = document.querySelector(`.films-list .films-list__container`);
+const filmsContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
 
 const filters = [
   {
@@ -55,36 +56,56 @@ const renderFilter = (filter) => {
 
 filters.forEach(renderFilter);
 
-const renderCards = (place, amount, cardArr) => {
-  cardArr.slice(0, amount).forEach((card) => {
-    const cardTemplateText = generateCard(card);
-    place.insertAdjacentHTML(`beforeend`, cardTemplateText);
-  });
-};
-
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-renderCards(mainCardContainer, 4, films);
-cardContainers.forEach((place) => {
-  renderCards(place, 2, films);
+const filterInputs = filterSection.querySelectorAll(`.main-navigation__item`);
+const filmComponents = films.map((film) => {
+  return new Film(film);
+});
+const commentsFilmComponents = films.map((film) => {
+  return new FilmComments(film);
 });
 
-const filterInputs = filterSection.querySelectorAll(`.main-navigation__item`);
+
+filmComponents.forEach((filmComponent) => {
+  filmsMainContainer.appendChild(filmComponent.render());
+});
+const commentsContainer = document.querySelector(`body`);
+
+const renderCards = (place, amount, cardArr) => {
+  cardArr.slice(0, amount).forEach((film) => {
+    place.appendChild(film.render());
+  });
+};
 
 filterInputs.forEach((input) => {
   input.addEventListener(`click`, (evt) => {
     evt.preventDefault();
-    mainCardContainer.innerHTML = ``;
-    cardContainers.forEach((place) => {
+    filmsMainContainer.innerHTML = ``;
+    filmsContainers.forEach((place) => {
       place.innerHTML = ``;
     });
-    renderCards(mainCardContainer, getRandomInt(1, 10), films);
-    cardContainers.forEach((place) => {
-      renderCards(place, getRandomInt(1, 10), films);
+    renderCards(filmsMainContainer, getRandomInt(1, 10), filmComponents);
+    filmsContainers.forEach((place) => {
+      renderCards(place, getRandomInt(1, 10), filmComponents);
     });
   });
+});
+
+filmComponents.forEach((component, index) => {
+  component.onComments = () => {
+    commentsFilmComponents[index].render();
+    commentsContainer.appendChild(commentsFilmComponents[index].element);
+  };
+});
+
+commentsFilmComponents.forEach((component) => {
+  component.onClose = () => {
+    commentsContainer.removeChild(component.element);
+    component.unrender();
+  };
 });
